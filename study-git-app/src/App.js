e2282f7f0d04b39cff33950019610762ba787e9a
Yulatun <julialimbekov@gmail.com>
2022-07-08 14:20:49 +0300

    delete reportWebVitals.js and ApTtest

--- study-git-app/src/App.js
+++ study-git-app/src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [count, setCount] = useState(0);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            User number: {count}
+           
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
